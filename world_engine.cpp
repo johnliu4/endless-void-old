125b8a9a4830b34f48681866766f5e7bc7927a7a
John Liu <johnliu478@gmail.com>
2019-09-21 22:10:28 -0400

    Add GLEW and GLFW3.

--- world_engine.cpp
+++ world_engine.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 
+#include <glew/glew.h>
+#include <glfw/glfw3.h>
+
 int main() {
-	std::cout << "Hello world!" << std::endl;
+	glewExperimental = true;
+	if (!glfwInit()) {
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "Hello world!", nullptr, nullptr);
+	if (!window) {
+		std::cout << "Failed to create GLFW window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	GLenum err = glewInit();
+	if (err != GLEW_OK) {
+		std::cout << "Failed to init GLEW." << std::endl;
+		return -1;
+	}
+
+	bool running = true;
+	while (running) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		if (glfwWindowShouldClose(window)) {
+			running = false;
+		}
+	}
+
+	glfwTerminate();
+	window = nullptr;
+	return 0;
 }
